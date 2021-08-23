df490ed53456ee80dea085931d6ea1d1d284716a
sylvain.martinez <sylvain.martinez@external.total.com>
2021-08-23 15:48:10 +0200

    wip

--- src/GetSummary.ps1
+++ src/GetSummary.ps1
@@ -5,6 +5,6 @@ param(
 
 Import-Module .\src\training.psm1
 $trainingSummary = Get-TrainingSummary -issueMarkdownContent $issueMarkdownContent
-$jsonTrainingSummary = $trainingSummary | ConvertTo-Json -Depth 10 -Compress
+$jsonTrainingSummary = $trainingSummary | ConvertTo-Json -Depth 10
 Write-Output $jsonTrainingSummary
 Write-Host "::set-output name=trainingSummary::$jsonTrainingSummary"
