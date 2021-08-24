2debbf75cccdc999f8d7547a722c6cbdde8cddb6
sylvain.martinez <sylvain.martinez@external.total.com>
2021-08-24 13:47:28 +0200

    wip

--- src/GetSummary.ps1
+++ src/GetSummary.ps1
@@ -8,7 +8,17 @@ param(
 )
 
 Import-Module .\src\training.psm1
+
 $trainingSummary = Get-TrainingSummary -issueMarkdownContent $issueMarkdownContent -issueUrl $issueUrl
+$configuration = Get-Content .\src\configuration.json | ConvertFrom-Json
+$trainerInfo = $configuration.trainers | Where-Object {$_.email -eq $traininSummary.trainer}
+if($trainerInfo) {
+    Write-Host "::set-output name=trainerUsername::$($trainerInfo.githubUsername)"
+}
+else {
+    Write-Warning "Can't fin trainer $($traininSummary.trainer). Won't be able to assign PR."
+    Write-Host "::set-output name=trainerUsername::''"
+}
 Write-Host "::set-output name=trainerEmail::$($trainerSummary.trainer)"
 $trainingSummary | ConvertTo-Json -Depth 10 | Out-File "$issueName.json"
-# Write-Output $jsonTrainingSummary
+
