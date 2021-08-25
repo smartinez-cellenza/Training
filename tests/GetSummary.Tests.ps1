ef1b5c0c2f80ab5c31f7b6e825bd66ceb36af8ef
smartinez-cellenza <73281539+smartinez-cellenza@users.noreply.github.com>
2021-08-25 15:35:14 +0200

    feat(ci): add linter (#16)
    * feat(ci): add linter
    
    * feat(ci): add linter
    
    * feat(ci): add linter
    
    Co-authored-by: sylvain.martinez <sylvain.martinez@external.total.com>

--- tests/GetSummary.Tests.ps1
+++ tests/GetSummary.Tests.ps1
@@ -13,51 +13,44 @@ Describe "GetSummary tests" -Tag "UnitTests" {
             Mock Write-Host {}
             Mock Write-Warning {}
             Mock Out-File {}
-        }
 
-        It "Should set githubUsername of trainer" {
-            Mock Get-TrainingSummary {return @{
+            $knownTrainerSummary = @{
                 trainer = "sylvain.martinez@cellenza.com";
                 trainees = @("first@trainee.com", "second@trainee.com");
                 trainingType = "Terraform";
                 issueUrl = "https://github.com/issues/42"
-            }}
+            }
+
+            $unKnownTrainerSummary = @{
+                trainer = "maistesquienfait@ettuviensdou.com";
+                trainees = @();
+                trainingType = "Crochet";
+                issueUrl = "https://github.com/issues/77"
+            }
+
+
+        }
+
+        It "Should set githubUsername of trainer" {
+            Mock Get-TrainingSummary {return $knownTrainerSummary}
             .\src\GetSummary.ps1 -issueMarkdownContent "lol" -issueName "42" -issueUrl "ici"
             Assert-MockCalled Write-Host -Exactly 1 -Scope It -ParameterFilter { $Object -eq "::set-output name=trainerUsername::smartinez-cellenza" }
         }
 
         It "Should warn when githubUsername not found" {
-            $trainerEmail = "maistesquienfait@ettuviensdou.com"
-            Mock Get-TrainingSummary {return @{
-                trainer = $trainerEmail;
-                trainees = @("first@trainee.com", "second@trainee.com");
-                trainingType = "Terraform";
-                issueUrl = "https://github.com/issues/42"
-            }}
+            Mock Get-TrainingSummary {return $unKnownTrainerSummary}
             .\src\GetSummary.ps1 -issueMarkdownContent "lol" -issueName "42" -issueUrl "ici"
-            Assert-MockCalled Write-Warning -Exactly 1 -Scope It -ParameterFilter {  $Message -eq "Can't find trainer $($trainerEmail). Won't be able to assign PR." }
+            Assert-MockCalled Write-Warning -Exactly 1 -Scope It -ParameterFilter {  $Message -eq "Can't find trainer maistesquienfait@ettuviensdou.com. Won't be able to assign PR." }
         }
 
         It "Should set trainer email" {
-            $trainerEmail = "maistesquienfait@ettuviensdou.com"
-            Mock Get-TrainingSummary {return @{
-                trainer = "sylvain.martinez@cellenza.com";
-                trainees = @("first@trainee.com", "second@trainee.com");
-                trainingType = "Terraform";
-                issueUrl = "https://github.com/issues/42"
-            }}
+            Mock Get-TrainingSummary {return $knownTrainerSummary}
             .\src\GetSummary.ps1 -issueMarkdownContent "lol" -issueName "42" -issueUrl "ici"
             Assert-MockCalled Write-Host -Exactly 1 -Scope It -ParameterFilter { $Object -eq "::set-output name=trainerEmail::sylvain.martinez@cellenza.com" }
         }
 
         It "Should create training file" {
-            $trainerEmail = "maistesquienfait@ettuviensdou.com"
-            Mock Get-TrainingSummary {return @{
-                trainer = "sylvain.martinez@cellenza.com";
-                trainees = @("first@trainee.com", "second@trainee.com");
-                trainingType = "Terraform";
-                issueUrl = "https://github.com/issues/42"
-            }}
+            Mock Get-TrainingSummary {return $knownTrainerSummary}
             .\src\GetSummary.ps1 -issueMarkdownContent "lol" -issueName "42" -issueUrl "ici"
             Assert-MockCalled Out-File -Exactly 1 -Scope It
         }
